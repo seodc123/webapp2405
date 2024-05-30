658dc0abdca0bf748d7a084e5f9fc0f04ac2054b
seodc123 <seodc123@naver.com>
2024-05-30 15:38:38 +0900

    Update webProgram.py

--- webProgram.py
+++ webProgram.py
@@ -1,5 +1,6 @@
 # 설치 필요
-# pip install langchain
+# pip install langchain streamlit
+
 import streamlit as st
 from langchain_community.llms import OpenAI
 
@@ -8,14 +9,13 @@ st.title('🍎🍐🍊 나의 AI Chat 🥝🍅🍆')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, max_tokens=500)  # max_tokens 값을 늘려 더 긴 응답을 받도록 설정
+    st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', '무엇을 도와드릴까요?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('OpenAI API 인증키를 입력해 주세요!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    
-    generate_response(text)
+    text = st.text_area('Enter text:', '무엇을 도와드릴까요?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('OpenAI API 인증키를 입력해 주세요!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
